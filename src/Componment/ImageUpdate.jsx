2839a16831fbc2148980a340c8811b9ea336868e
Minji Kim <110662771+sloane323@users.noreply.github.com>
2023-02-03 14:19:41 +0900

    new

--- src/Componment/ImageUpdate.jsx
+++ src/Componment/ImageUpdate.jsx
@@ -1,63 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { db, storage } from '../firebase';
+import React, { useState, useEffect } from "react";
+import { storage } from "../firebase";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
 
 const ImageUpdate = () => {
-    const [attachment, setAttachment] = useState() ;
-    const [file, setFile] = useState('')
-    const code = Math.random().toString(36);
-    const userkey = localStorage.getItem("currentUser"); 
-
-    const onFileChange = (e) => {
-        const { files } = e.target;
-        const theFile = files[0];
-        // FileReader의 Instance 생성
-        const reader = new FileReader();
-        // eventListener
-        reader.onloadend = (e) => {
-          const { result } = e.currentTarget;
-          // attachment state를 data_url형식의 string값으로 업데이트
-          setAttachment(result);
-        };
-        reader.readAsDataURL(theFile);
-      };
-      
-      const onSubmit = async (e) => {
-        e.preventDefault();
-        let attachmentUrl = '';
-        // 사진 첨부를 안할 수도 있기 때문에 조건문
-        if (attachment !== '') {
-          // uuid library 사용
-          const fileRef = storage.ref().child(`${userkey}/${code()}`);
-          // putString method로 file 업로드
-          const response = await fileRef.putString(attachment, 'data_url');
-          // getDownloadURL method로 file 다운로드
-          attachmentUrl = await response.ref.getDownloadURL();
+    const [image, setImage] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [error, setError] = useState("");
+    const [progress, setProgress] = useState(100);
+  
+    const handleImage = (event) => {
+      const image = event.target.files[0];
+      setImage(image);
+      console.log(image);
+      setError("");
+    };
+  
+    const onSubmit = (event) => {
+      event.preventDefault();
+      setError("");
+      if (image === "") {
+        console.log("파일이 선택되지 않았습니다");
+        setError("파일이 선택되지 않았습니다");
+        return;
+      }
+      // 업로드 처리
+      console.log("업로드 처리");
+      const storageRef = storage.ref("images/test/"); //어떤 폴더 아래에 넣을지 설정
+      const imagesRef = storageRef.child(image.name); //파일명
+  
+      console.log("파일을 업로드하는 행위");
+      const upLoadTask = imagesRef.put(image);
+      console.log("태스크 실행 전");
+  
+      upLoadTask.on(
+        "state_changed",
+        (snapshot) => {
+          console.log("snapshot", snapshot);
+          const percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(percent + "% done");
+          setProgress(percent);
+        },
+        (error) => {
+          console.log("err", error);
+          setError("파일 업로드에 실패했습니다." + error);
+          setProgress(100); //진행중인 바를 삭제
+        },
+        () => {
+          upLoadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            setImageUrl(downloadURL);
+          });
         }
-        const messageObj = {
-          createdAt: Date.now(),
-          creatorId: userkey,
-          attachmentUrl,
-        };
-        await db.collection('postse').add(messageObj);
-        setAttachment('');
-      };
+      );
+    };
 
-    return ( <div>
-        
+    return ( 
         <div>
-
-        <input type="file" accept="image/*" onChange={onFileChange} value={file}/>
-        <button onClick={onsubmit}> 제출하기 </button> 
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" alt="attachment"/>
-          </div>
-        )}
-
-
+           
+     
+        <input type="file" onChange={handleImage} />
+        <button onClick={onSubmit}>업로드</button>
+     
+     
+      {imageUrl && (
+        <div>
+          <img width="400px" src={imageUrl} alt="uploaded" />
+        </div>
+      )}
     </div>
-
-    </div> );
+            
+   
+     );
 }
  
 export default ImageUpdate;
